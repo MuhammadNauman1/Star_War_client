c7e66c76bdc2002a96e1fc570e1c3333de7a6208
Chaudhary-Hassnain <tajammalhusnain1996@gmail.com>
2020-03-10 18:26:00 +0500

    update file

--- Star_War/ClientApp/src/App.js
+++ Star_War/ClientApp/src/App.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
-import './custom.css'
-
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+import React from 'react';
+import './App.css';
+import QnA from './components/qna.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Assets/css/style.css'
+import logo from './Assets/logo/Star_Wars_Logo.svg'
+class App extends React.Component 
+{
+  constructor(props)
+  {
+    super(props);
+    this.state={isLoggedIn:false,data:[]};
+    this.fetchQnA=this.fetchQnA.bind(this);
+  }
+  fetchQnA() {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' ,'Authorization' :'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlNhbXBsZSBBcHAiLCJhcHBUb2tlbiI6IjkwYmNmZDAxMWZjZDQ2NmVhNWJkNmE0Y2I5MTY3NDU3IiwibmJmIjoxNTgzMzQ5MjQyLCJleHAiOjE1ODU5NDEyNDIsImlhdCI6MTU4MzM0OTI0MiwiaXNzIjoiZXhwbG9yZS1jYWxpLWlzc3VlciIsImF1ZCI6ImV4cGxvcmUtY2FsaS1hdWRpZW5jZSJ9.2CFeL0vFQTWM0vZ1lzfK8a3R5vzGG4HdnlymN6GhHi0'}
+  };
+  fetch(`http://localhost:52201/api/Stars`, requestOptions).then(data=>data.json()).then(data=>{
+   
+      this.setState({data:data});
+      this.setState({isLoggedIn:true});
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
+  render() {
+    let qnA=null;
+    if(this.state.isLoggedIn){
+      qnA= <QnA testData={this.state.data}/>;
+    }
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      <div className="mainBody">
+        <img src={logo} height="300" width="300" alt="Kiwi standing on oval" />
+        <br/>
+           <button type="button" className="btn-yellow button btn-sm"  onClick={this.fetchQnA}>
+            <span className="glyphicon glyphicon-star"></span> Do. Or do not. There is no try. <span className="glyphicon glyphicon-star"></span>
+          </button>
+          <br/>
+          <br/>
+       {qnA}
+      </div>
     );
   }
+
 }
+
+
+export default App;
--- Star_War/ClientApp/src/index.js
+++ Star_War/ClientApp/src/index.js
@@ -1,18 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css";
+// import "https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
+// import './Assets/js/jquery.min.js'
+// import './Assets/js/bootstrap.min.js'
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
-
-registerServiceWorker();
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
